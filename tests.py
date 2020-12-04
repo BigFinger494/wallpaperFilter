daa75788001b4be180c02dbdadb5f4f5bc629683
BigFinger494 <bigfinger494@gmail.com>
2020-12-04 22:57:57 +0300

    ultimate update

--- tests.py
+++ tests.py
@@ -1,17 +1,22 @@
 import unittest
-import ezFilter as tested_app
+import ezFilter  as tested_app
 import json
 import os.path
+import argparse
+import sys
 class FilterScriptTests(unittest.TestCase):
-
+    
+    script = "jemima"
+    path = "\\newFolder"
     def setUp(self):
         tested_app.first= "\\newFolder"
-        tested_app.script = ""
+        tested_app.script = "\\newFolder"
        
 
-    def test_create_new_folder(self):
-        path = tested_app.createDirectory()
+    def test_create_new_folderTest(self):
+        path = tested_app.createDirectory("\\newFoldah")
         self.assertTrue(os.path.exists(path))
+
 
 
 if __name__ == '__main__':
